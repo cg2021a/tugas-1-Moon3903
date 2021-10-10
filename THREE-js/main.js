3681758be40d00c87e0087db6bc41099be191a83
unknown <zulfikar.fauzul.akbar@gmail.com>
2021-10-10 22:12:54 +0700

    test wireframe and add circle

--- THREE-js/main.js
+++ THREE-js/main.js
@@ -4,14 +4,29 @@ const canvas = document.getElementById("myCanvas");
 
 const scene = new THREE.Scene();
 
-const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight,
-    1, 1000);
+const camera = new THREE.PerspectiveCamera(30, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.z = 5;
 
-const geometry = new THREE.BoxGeometry(0.3, 0.3, 0.3);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// cube
+const geometry = new THREE.BoxGeometry(0.5, 0.5, 0.5);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
+cube.position.x += -1.6;
+cube.position.y += 0.8;
 scene.add(cube);
+
+// cube wireframe
+const wmaterial = new THREE.MeshBasicMaterial({ color: 0x000000, wireframe: true});
+const wcube = new THREE.Mesh(geometry, wmaterial);
+wcube.position.x += -1.6;
+wcube.position.y += 0.8;
+scene.add(wcube);
+
+// circle
+const cgeometry = new THREE.CircleGeometry( 0.5, 32 );
+const cmaterial = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const circle = new THREE.Mesh( cgeometry, cmaterial );
+scene.add( circle );
 
 const renderer = new THREE.WebGLRenderer({canvas: canvas,alpha: true});
 renderer.setSize(0.9 * window.innerWidth, 0.75 * window.innerHeight);
